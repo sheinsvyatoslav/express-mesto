dabf5af58fa2be8b3cc90d75bd4e71eb910bcc7e
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-03-17 00:11:01 +0300

    bugs fixed

--- app.js
+++ app.js
@@ -14,10 +14,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
-
-app.use(cors());
 
 app.use(requestLogger);
 app.get('/crash-test', () => {
