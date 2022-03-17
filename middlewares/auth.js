bdd9ccde58612d03416d7150f9960a46aab90ca9
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-03-17 20:03:33 +0300

    Добавляет валидацию email

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,7 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 
 const { JWT_SECRET = 'dev-secret' } = process.env;
 
+/*
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
@@ -14,5 +15,27 @@ module.exports = (req, res, next) => {
     throw new UnauthorizedError('Необходима авторизация');
   }
   req.user = payload;
+  next();
+};
+*/
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  req.user = payload;
+
   next();
 };
