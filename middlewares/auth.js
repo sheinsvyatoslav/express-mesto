a8557c30677be6c188a3cc62a45d3357bdb83456
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-03-17 21:28:11 +0300

    bugs fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,16 +8,18 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизациbb');
+    throw new UnauthorizedError('Необходима авторизаци');
   }
 
   const token = authorization.replace('Bearer ', '');
+  console.log(token);
+  console.log(JWT_SECRET);
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизацияqq');
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   req.user = payload;
